9d893417635371edaf2a327a7e2e8b05442bf74e
calvuS42 <o.solonyi@gmail.com>
2021-10-11 01:12:22 +0300

    add fourth image processing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def process_first_img(img_name):
     img = cv2.imread(img_name)
@@ -73,7 +73,51 @@ def process_third_img(img_name):
     cv2.imwrite('../hw_output/img3_res.jpg', img_v)
 
 
+def process_fourth_img(img_name):
+    img = cv2.imread(img_name)
+    cv2.imshow('Raw image', img)
+    cv2.waitKey(0)
+
+    (B, G, R) = cv2.split(img)
+    (B, G, R) = (np.float32(B), np.float32(G), np.float32(R))
+    diff = G - (B + R)
+    cv2.imshow('Diff img', diff)
+    cv2.waitKey(0)
+
+    ran = cv2.inRange(diff, -80, 100)
+    ran = 255 - ran
+    cv2.imshow('In range img', ran)
+    cv2.waitKey(0)
+
+    img_v = cv2.erode(ran, np.ones((3, 3), np.uint8), iterations=1)
+    cv2.imshow('Result', img_v)
+    cv2.waitKey(0)
+
+    cv2.imwrite('../hw_output/img4_res.jpg', img_v)
+
+
+def show_img_by_channels(img):
+    """
+    Function which draws all channels as a wider single-channel image.
+    :return: No return
+    """
+
+    # Create an empty image (numpy array) for wide image result
+    img_split_ch = np.zeros((img.shape[0], img.shape[1] * img.shape[2]), dtype=np.uint8)
+
+    # Filling the empty image with channels intensities
+    for i in range(img.shape[2]):
+        img_split_ch[:, img.shape[1] * i: img.shape[1] * (i + 1)] = img[:, :, i]
+
+    # Show the channel-separated image
+    cv2.imshow('Three channels of color image in its color space', img_split_ch)
+
+    # Waiting for a key (milliseconds to wait or 0 for infinite wait for a key stroke)
+    cv2.waitKey(0)  # won't draw anything without this function!!!
+
+
 if __name__ == '__main__':
     # process_first_img('../Images for home task #1/hearts 1.png')
     # process_second_img('../Images for home task #1/hearts 2.png')
-    process_third_img('../Images for home task #1/hearts 3.png')
+    # process_third_img('../Images for home task #1/hearts 3.png')
+    process_fourth_img('/home/calvus/It-Jim course/les-01/Images for home task #1/hearts 4.png')
