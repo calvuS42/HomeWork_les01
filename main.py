2e568260e5594bf21717c76e7ecaa3107846a75d
calvuS42 <o.solonyi@gmail.com>
2021-10-10 20:59:04 +0300

    add second image processing

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def process_first_img(img_name):
 
     cv2.imwrite('../hw_output/img1_res.jpg', img_v)
 
+
 def process_second_img(img_name):
     img = cv2.imread(img_name)
     cv2.imshow('Raw image', img)
@@ -31,18 +32,21 @@ def process_second_img(img_name):
     cv2.imshow('Gray img', img_gray)
     cv2.waitKey(0)
 
-    img_thresh = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 65, 1)
+    img_median = cv2.medianBlur(img_gray, 5)
+    cv2.imshow('Equalized', img_median)
+    cv2.waitKey(0)
+
+    img_thresh = cv2.adaptiveThreshold(img_median, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 105, 1.9)
     cv2.imshow('Thresholded', img_thresh)
     cv2.waitKey(0)
 
-    img_v = cv2.dilate(img_thresh, np.ones((5, 5), np.uint8), iterations=1)
-    img_v = cv2.erode(img_v, np.ones((9, 9), np.uint8), iterations=1)
+    img_v = cv2.erode(img_thresh, np.ones((3, 3), np.uint8), iterations=1)
     cv2.imshow('Result', img_v)
     cv2.waitKey(0)
 
-    # cv2.imwrite('../hw_output/img2_res.jpg', img_v)
+    cv2.imwrite('../hw_output/img2_res.jpg', img_v)
 
 
 if __name__ == '__main__':
     # process_first_img('../Images for home task #1/hearts 1.png')
-    process_second_img('/home/calvus/It-Jim course/les-01/Images for home task #1/hearts 2.png')
+    process_second_img('../Images for home task #1/hearts 2.png')
